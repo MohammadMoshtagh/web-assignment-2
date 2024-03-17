e8ba0dd7b137756109432f2c250cccd9bbbd6b24
Mohammad <mhmmoshtaghi@gmail.com>
2024-03-18 01:28:20 +0330

    Add get endpoint

---
+++ src/main/java/edu/webclass/restapi/Product/Management/System/services/ProductService.java
@@ -0,0 +1,34 @@
+package edu.webclass.restapi.Product.Management.System.services;
+
+import edu.webclass.restapi.Product.Management.System.models.Product;
+import edu.webclass.restapi.Product.Management.System.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAllProducts(){
+        return productRepository.findAllProducts();
+    }
+
+    public Product findProductById(String id) {
+        var product = productRepository.findProductById(id);
+        if (product != null) {
+            return product;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product with id %s not found".formatted(id));
+        }
+    }
+
+    public boolean addProduct(String title, String brand, int price){
+        return productRepository.createNewProduct(new Product(title,brand,price));
+    }
+}
